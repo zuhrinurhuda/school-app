3029d6d88c56edcd4ff4dd994a07e8ec30a96e05
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-10 14:53:04 +0700

    revisi

--- app.js
+++ app.js
@@ -33,5 +33,6 @@ app.use('/', subject)
 const student = require('./routers/student')
 app.use('/', student)
 
-app.listen(3000)
-console.log('Listening on port 3000')
+// app.listen(3000)
+// console.log('Listening on port 3000')
+app.listen(process.env.PORT || '3000')
